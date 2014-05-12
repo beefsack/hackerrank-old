c7e81cd8a3c44bfc1aeff5cdba566c4818888a35
Michael Alexander <beefsack@gmail.com>
2014-05-12 21:37:38 +1000

    Added a base to start from.

---
+++ base/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print(run(os.Stdin))
+}
+
+func run(input io.Reader) string {
+	outputs := []string{}
+	b, _ := ioutil.ReadAll(input)
+	lines := strings.Split(strings.TrimSpace(string(b)), "\n")
+	for _, _ = range lines {
+		// Logic goes here
+	}
+	return strings.Join(outputs, "\n")
+}
+
+func parseInts(input string) []int {
+	strInts := regexp.MustCompile(`\s+`).Split(strings.TrimSpace(input), -1)
+	ints := make([]int, len(strInts))
+	for k, s := range strInts {
+		i, _ := strconv.Atoi(s)
+		ints[k] = i
+	}
+	return ints
+}
