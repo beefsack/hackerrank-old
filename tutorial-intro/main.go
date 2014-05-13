6e6b7c3a98ad5f4543979c3b2a8052c8d0128ae5
Michael Alexander <beefsack@gmail.com>
2014-05-13 22:46:38 +1000

    Completed tutorial-intro.

---
+++ tutorial-intro/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print(run(os.Stdin))
+}
+
+func run(input io.Reader) string {
+	b, _ := ioutil.ReadAll(input)
+	lines := strings.Split(strings.TrimSpace(string(b)), "\n")
+	search, _ := strconv.Atoi(lines[0])
+	nums := parseInts(lines[2])
+	for k, l := range nums {
+		if l == search {
+			return strconv.Itoa(k)
+		}
+	}
+	return "-1"
+}
+
+func parseInts(input string) []int {
+	strInts := regexp.MustCompile(`\s+`).Split(strings.TrimSpace(input), -1)
+	ints := make([]int, len(strInts))
+	for k, s := range strInts {
+		i, _ := strconv.Atoi(s)
+		ints[k] = i
+	}
+	return ints
+}
---
+++ tutorial-intro/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+type Case struct {
+	Input, Expected string
+}
+
+func TestRun(t *testing.T) {
+	for _, c := range []Case{
+		Case{
+			Input: `4
+6
+1 4 5 7 9 12`,
+			Expected: `1`,
+		},
+	} {
+		actual := run(bytes.NewBufferString(c.Input))
+		if actual != c.Expected {
+			t.Fatal(actual)
+		}
+	}
+}
